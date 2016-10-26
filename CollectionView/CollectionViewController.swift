8176670c3b55e39d285aae009adbd76cd2688d48
Tarang Hirani <taranghirani@Tarangs-MacBook-Pro.local>
2016-10-27 01:01:23 +0530

    Initial commit for UICollectionView example

---
+++ CollectionView/CollectionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionViewController.swift
+//  CollectionView
+//
+//  Created by Tarang Hirani on 10/26/16.
+//  Copyright © 2016 Tarang Hirani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
