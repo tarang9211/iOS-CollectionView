1bd0b5be747dbf867c1b78715c25e906cbb0483a
Tarang Hirani <taranghirani@Tarangs-MacBook-Pro.local>
2016-10-27 01:03:29 +0530

    Added custom cell for the collection view

---
+++ CollectionView/CustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCell.swift
+//  CollectionView
+//
+//  Created by Tarang Hirani on 10/27/16.
+//  Copyright © 2016 Tarang Hirani. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+    
+}
