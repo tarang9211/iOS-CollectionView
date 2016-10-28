efc25148bfeecb04c1785f1789799acfb04b738c
Tarang Hirani <taranghirani@Tarangs-MacBook-Pro.local>
2016-10-29 01:08:29 +0530

    Added header view, fixed corner radius and shadows for each cell

--- CollectionView/CustomCell.swift
+++ CollectionView/CustomCell.swift
@@ -10,23 +10,25 @@ import UIKit
 
 class CustomCell: UICollectionViewCell {
     
+    @IBOutlet weak var headerViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var headerView: UIView!
     
     func setUpCell() {
  
-        self.backgroundColor = UIColor.white
-        self.layer.cornerRadius = 5
-        self.layer.masksToBounds = false
+        //set up for corner radius and shadow
+        self.contentView.layer.backgroundColor = UIColor.white.cgColor
+        self.contentView.layer.cornerRadius = 5
+        self.contentView.layer.masksToBounds = true;
         self.layer.shadowColor = UIColor.black.cgColor
-        self.layer.shadowOffset = CGSize(width: 0, height: 5)
+        self.layer.shadowOffset = CGSize(width:0, height: 2.0)
         self.layer.shadowOpacity = 0.2
+        self.layer.masksToBounds = false;
         
-        self.setUpHeaderView()
-    }
-    
-    func setUpHeaderView() {
-        self.clipsToBounds = true
+        //set up for header view
+        self.headerViewHeightConstraint.constant = self.frame.height * 0.25
+        self.headerView.backgroundColor = UIColor.red
+        self.updateConstraints()
     }
     
 }
