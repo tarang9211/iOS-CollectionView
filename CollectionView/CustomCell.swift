18c59d18c22bdc3980ba5012193797dcdffc23b0
Tarang Hirani <taranghirani@Tarangs-MacBook-Pro.local>
2016-10-28 11:18:30 +0530

    Commented code out

--- CollectionView/CustomCell.swift
+++ CollectionView/CustomCell.swift
@@ -11,4 +11,19 @@ import UIKit
 class CustomCell: UICollectionViewCell {
     
     @IBOutlet weak var title: UILabel!
+    
+//    override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+//        let attr = layoutAttributes.copy() as! UICollectionViewLayoutAttributes
+//        
+//        self.setNeedsLayout()
+//        self.layoutIfNeeded()
+//        
+//        let desiredHeight: CGFloat = 20
+//        let desiredWidth: CGFloat = 20
+//        attr.frame.size.height = desiredHeight
+//        attr.frame.size.width = desiredWidth
+//        self.frame = attr.frame  // Do NOT forget to set the frame or the layout won't get it !!!
+//        
+//        return attr
+//    }
 }
