6fbce5723c165b1f385f8b1ef9c274738fcbb4ef
Tarang Hirani <taranghirani@Tarangs-MacBook-Pro.local>
2016-11-03 00:53:13 +0530

    Added a new struct for faking data

---
+++ CollectionView/CardData.swift
@@ -0,0 +1,16 @@
+//
+//  CardData.swift
+//  CollectionView
+//
+//  Created by Tarang Hirani on 11/3/16.
+//  Copyright © 2016 Tarang Hirani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CardData {
+    var username: String!
+    var profileImage: UIImage
+    var time: String!
+}
